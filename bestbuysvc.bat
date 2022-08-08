fee7df63e26b1f3ffb28447b86768cc0e81b53bf
githlp69 <110198628+githlp69@users.noreply.github.com>
2022-08-08 14:50:08 -0400

    Add files via upload

---
+++ bestbuysvc.bat
@@ -0,0 +1,10 @@
+@echo off
+
+C:\windows\system32\rasdial.exe BestBuy darrindeyoung3 XC748B73
+powercfg -setactive 381b4222-f694-41f0-9685-ff5bb260df2e
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
+powercfg /change monitor-timeout-ac 0
+powercfg /change monitor-timeout-dc 0
+powercfg /change hibernate-timeout-ac 0
+powercfg /change hibernate-timeout-dc 0
